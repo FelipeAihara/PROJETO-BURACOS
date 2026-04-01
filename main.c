18987c000b124d146db1119eb64e3e10da13cb3c
FelipeAihara <fyaihara@gmail.com>
2026-04-01 12:52:48 -0300

    Refactor image reading and copying functions

--- main.c
+++ main.c
@@ -2,33 +2,35 @@
 #include <stdlib.h>
 
 int main(void) {
-    int lerImagem(char nome[], char tipo[], int *M, int *largura, int *altura, int *valorMax, int *valorMin);
-    
-    char tipo[3];
-    int *M, largura, altura, valorMax, valorMin;
-    char nome[] = "superficie_aleatoria.pgm";
+    void lerImagem(char nome[], char tipo[], int **M, int *largura, int *altura, int *valorMax, int *valorMin);
+    void criaCopia(char nome_arquivo[], int *M, int **C, int largura, int altura, int valorMax, int valorMin);
 
-    int a = lerImagem(nome, tipo, M, &largura, &altura, &valorMax, &valorMin);
-    if (a == 1) printf("Erro ao ler o arquivo");
+    char tipo[3];
+    int *M, *C, largura, altura, valorMax, valorMin;
+    char original[] = "superficie_aleatoria.pgm";
+    char copia[] = "copia_superficie_aleatoria.pgm";
+
+    lerImagem(original, tipo, &M, &largura, &altura, &valorMax, &valorMin);
     
     printf("Largura: %d\n", largura);
     printf("Altura: %d\n", altura);
     printf("Valor maximo: %d\n", valorMax);
     printf("Valor minimo: %d\n", valorMin);
 
+    criaCopia(copia, M, &C, largura, altura, valorMax, valorMin);
+
     return 0;
 }
 
-// Retorna 0 se dá tudo certo. Retorna outro número caso contrário.
-int lerImagem(char nome[], char tipo[], int *M, int *largura, int *altura, int *valorMax, int *valorMin) {
+void lerImagem(char nome_arquivo[], char tipo[], int **M, int *largura, int *altura, int *valorMax, int *valorMin) {
     
-    // Abrir o arquivo
-    FILE *arquivo = fopen(nome, "r");
+    // Abre o arquivo
+    FILE *arquivo = fopen(nome_arquivo, "r");
     if (arquivo == NULL) {
-        return 1;
+        return;
     }
 
-    // Ler o tipo da imagem
+    // Lê o tipo da imagem
     fscanf(arquivo, "%s", tipo);
 
     // Ler largura, altura e valor máximo
@@ -36,20 +38,58 @@ int lerImagem(char nome[], char tipo[], int *M, int *largura, int *altura, int *
     fscanf(arquivo, "%d", valorMax);
 
     // Cria uma matriz com as dimensões da imagem
-    M = (int *)malloc((*largura) * (*altura) * sizeof(int));
-    if (M == NULL) {
+    int *temp = (int *)malloc((*largura) * (*altura) * sizeof(int));
+    *M = temp;
+    if (*M == NULL) {
             printf("Erro de memória\n");
-            return 1;
+            return;
         }
 
-    // Ler os pixels e achar o valor mínimo
+    // Lê os pixels e achar o valor mínimo
     *valorMin = 255;
     for (int i = 0; i < *altura; i++) {
         for (int j = 0; j < *largura; j++) {
-            fscanf(arquivo, "%d", &M[i*(*largura) + j]);
-            if (M[i*(*largura) + j] < *valorMin) *valorMin = M[i*(*largura) + j];
+            fscanf(arquivo, "%d", &temp[i*(*largura) + j]);
+            if (temp[i*(*largura) + j] < *valorMin) *valorMin = temp[i*(*largura) + j];
         }
     }
     fclose(arquivo);
-    return 0;
+}
+
+// Cria um arquivo PGM contendo uma cópia da imagem já processada
+void criaCopia(char nome_arquivo[], int *M, int **C, int largura, int altura, int valorMax, int valorMin) {
+
+    // Cria um arquivo
+    FILE *arquivo = fopen(nome_arquivo, "w");
+    if (arquivo == NULL) {
+        printf("Erro ao criar o arquivo");
+        return;
+    }
+
+    // Escreve o cabeçalho do formato PGM
+    fprintf(arquivo, "P2\n");
+    fprintf(arquivo, "512 512\n");
+    fprintf(arquivo, "255\n");
+
+    // Cria uma cópia já processada da imagem
+    int *temp = (int *)malloc(largura * altura * sizeof(int));
+    *C = temp;
+    if (M == NULL) {
+            printf("Erro de memória\n");
+            return;
+        }
+
+    int cota = (valorMax + valorMin) / 2;
+
+    for (int i = 0; i < altura; i++) {
+        for (int j = 0; j < largura; j++) {
+            int m = M[i*altura + j];
+            if (m <= cota) m = 0;
+            else m = 255;
+            temp[i*altura + j] = m;
+            fprintf(arquivo, "%d ", m);
+        }
+        fprintf(arquivo, "\n");
+    }
+    fclose(arquivo);
 }
