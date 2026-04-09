68545021a84d6c0c4c07dc289360b8ecf1500112
FelipeAihara <fyaihara@gmail.com>
2026-04-09 12:50:45 -0300

    Update image processing and clustering logic
    Refactor image processing functions and integrate clustering.

--- main.c
+++ main.c
@@ -1,99 +1,40 @@
+/* NOTAS:
+- Usar int ao invés de float pode resultar em erros. Ficar atento a essa possibilidade
+- printf("Passou aqui\n");
+- É ineficiente usar a sqrt na função clustering mas vou usar
+- Vamos assumir que se d(a,b) < r e d(b,c) < r -> d(a,c) < r
+- Esse algoritmo não funciona para caso haja buracos muitos pequenos muito próximos
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
+#include "imagem.h"
+#include "clustering.h"
 
-void lerImagem(char nome[], char tipo[], int **M, int *largura, int *altura, int *valorMax, int *valorMin);
-void criaCopia(char nome_arquivo[], int *M, int **C, int largura, int altura, int valorMax, int valorMin);
+#define K 30
+#define raio_quadrado 20
 
 int main(void) {
 
     char tipo[3];
     int *M, *C, largura, altura, valorMax, valorMin;
+    int contador = 0;
     char original[] = "superficie_aleatoria.pgm";
     char copia[] = "copia_superficie_aleatoria.pgm";
 
     lerImagem(original, tipo, &M, &largura, &altura, &valorMax, &valorMin);
-    
-    printf("Largura: %d\n", largura);
-    printf("Altura: %d\n", altura);
-    printf("Valor maximo: %d\n", valorMax);
-    printf("Valor minimo: %d\n", valorMin);
 
-    criaCopia(copia, M, &C, largura, altura, valorMax, valorMin);
+    criaCopia(copia, M, &C, largura, altura, valorMax, valorMin, &contador);
+
+    int tamanho = largura * altura;
+    Pontos pnt[contador];
+    Centroides ctr[K];
+    clustering(C, tamanho, pnt, contador, ctr, K);
+
+    printaBuracos(C, altura, ctr, K, raio_quadrado);
 
     free(M);
     free(C);
-
-    return 0;
-}
-
-void lerImagem(char nome_arquivo[], char tipo[], int **M, int *largura, int *altura, int *valorMax, int *valorMin) {
     
-    // Abre o arquivo
-    FILE *arquivo = fopen(nome_arquivo, "r");
-    if (arquivo == NULL) {
-        return;
-    }
-
-    // Lê o tipo da imagem
-    fscanf(arquivo, "%s", tipo);
-
-    // Ler largura, altura e valor máximo
-    fscanf(arquivo, "%d %d", largura, altura);
-    fscanf(arquivo, "%d", valorMax);
-
-    // Cria uma matriz com as dimensões da imagem
-    int *temp = (int *)malloc((*largura) * (*altura) * sizeof(int));
-    *M = temp;
-    if (*M == NULL) {
-            printf("Erro de memória\n");
-            return;
-        }
-
-    // Lê os pixels e achar o valor mínimo
-    *valorMin = 255;
-    for (int i = 0; i < *altura; i++) {
-        for (int j = 0; j < *largura; j++) {
-            fscanf(arquivo, "%d", &temp[i*(*largura) + j]);
-            if (temp[i*(*largura) + j] < *valorMin) *valorMin = temp[i*(*largura) + j];
-        }
-    }
-    fclose(arquivo);
-}
-
-// Cria um arquivo PGM contendo uma cópia da imagem já processada
-void criaCopia(char nome_arquivo[], int *M, int **C, int largura, int altura, int valorMax, int valorMin) {
-
-    // Cria um arquivo
-    FILE *arquivo = fopen(nome_arquivo, "w");
-    if (arquivo == NULL) {
-        printf("Erro ao criar o arquivo");
-        return;
-    }
-
-    // Escreve o cabeçalho do formato PGM
-    fprintf(arquivo, "P2\n");
-    fprintf(arquivo, "512 512\n");
-    fprintf(arquivo, "255\n");
-
-    // Cria uma cópia já processada da imagem
-    int *temp = (int *)malloc(largura * altura * sizeof(int));
-    *C = temp;
-    if (M == NULL) {
-            printf("Erro de memória\n");
-            return;
-        }
-
-    int cota = (valorMax + valorMin) / 2;
-
-    for (int i = 0; i < altura; i++) {
-        for (int j = 0; j < largura; j++) {
-            int m = M[i*altura + j];
-            if (m <= cota) m = 0;
-            else m = 255;
-            temp[i*altura + j] = m;
-            fprintf(arquivo, "%d ", m);
-        }
-        fprintf(arquivo, "\n");
-    }
-    fclose(arquivo);
+    return 0;
 }
